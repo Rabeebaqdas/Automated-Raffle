84cd6d1185e46ec1e660c374a4bb118627bab73a
Rabeeb Aqdas Jilani <63879512+Rabeebaqdas@users.noreply.github.com>
2023-02-02 13:40:21 +0500

    Update hardhat.config.js

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -6,7 +6,7 @@ require("hardhat-gas-reporter");
 require("hardhat-contract-sizer");
 require("dotenv").config();
 
-  const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL;
+  const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
   const PRIVATE_KEY = process.env.PRIVATE_KEY;
   const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY; 
 
@@ -20,16 +20,16 @@ module.exports = {
     localhost: {
       chainId: 31337,
   },
-    rinkeby : {
-      chainId : 4,
+    goerli : {
+      chainId : 5,
       blockConfirmations : 6,
-      url:RINKEBY_RPC_URL,
+      url:GOERLI_RPC_URL,
       accounts : [PRIVATE_KEY]
     }
   },
   etherscan : {
     apiKey : {
-      rinkeby : ETHERSCAN_API_KEY
+      goerli : ETHERSCAN_API_KEY
     }
   },
   gasReporter:{
